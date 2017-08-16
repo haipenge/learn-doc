9f10f184c16c74f5d9b4a2bc2a731df5fdf4a71b
haipenge@gmail.com <haipenge@gmail.com>
2017-08-16 11:26:17 +0800

    local add

--- mycat/scripts/create_multi_dbs.py
+++ mycat/scripts/create_multi_dbs.py
@@ -14,6 +14,8 @@ class SchemaTools(object):
 		self.servers=['10.11.100.39','10.11.100.40','10.11.100.41','10.11.100.42']
 		self.test_servers=['10.1.5.105','10.1.5.250']
 		self.user_and_password={'user':'prnp','password':'prnp'}
+		self.test_user_and_password={'user':'prnp','password':'Mysql@105'}
+	#创建生产库，库名：dbs0-99,四台，每台25片
 	def generate_multi_db(self):
 		for i,ip in enumerate(self.servers):
 			print i,ip
@@ -26,15 +28,40 @@ class SchemaTools(object):
 				cmds.append(command)
 				print command
 				os.system(command)
+	#创建测试库，库名：dbs0-99,四台，每台25片
+	def generate_test_multi_db(self):
+		for i,ip in enumerate(self.test_servers):
+			print i,ip
+			user=self.test_user_and_password['user']
+			password=self.test_user_and_password['password']
+			cmds=[]
+			db_indexs=range(i*4,(i+1)*25)
+			for i in db_indexs:
+				command='mysql -u '+user+' -p'+password+' -h '+ip+' -e "create database dbs'+i+'"'
+				cmds.append(command)
+				print command
+				os.system(command)
+	#创建生产公共库与统计库
 	def generate_singel_db(self):
 		user=self.user_and_password['user']
 		password=self.user_and_password['password']
 		command='mysql -u '+user+' -p'+password+' -h '+self.servers[0]+' -e "create database nuc_pub"'
 		command='mysql -u '+user+' -p'+password+' -h '+self.servers[1]+' -e "create database nuc_stat"'
-	def generate(self):
-		self.generate_multi_db()
-		self.generate_singel_db()
+	#创建测试公共库，统计库
+	def generate_test_singel_db(self):
+		user=self.user_and_password['user']
+		password=self.test_user_and_password['password']
+		command='mysql -u '+user+' -p'+password+' -h '+self.test_servers[0]+' -e "create database nuc_pub"'
+		command='mysql -u '+user+' -p'+password+' -h '+self.test_servers[1]+' -e "create database nuc_stat"'
+	def generate(self,set_env):
+		if set_env == 'test':
+			self.generate_test_singel_db()
+			self.generate_test_multi_db()
+		else:
+			self.generate_multi_db()
+			self.generate_singel_db()
 
 if __name__=='__main__':
 	tools=SchemaTools()
-	tools.generate()
+	set_env=raw_input('初始化环境[test,prod]:')
+	tools.generate(set_env)
