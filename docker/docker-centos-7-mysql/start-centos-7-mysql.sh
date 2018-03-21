71ef6c23d559ef39c82c5c4825b99d416925126d
haipenge@gmail.com <haipenge@gmail.com>
2018-03-21 10:20:37 +0800

    增加galera cluster

--- docker/docker-centos-7-mysql/start-centos-7-mysql.sh
+++ docker/docker-centos-7-mysql/start-centos-7-mysql.sh
@@ -3,6 +3,6 @@
 #author:haipenge
 #date:2018.02.02
 echo '>>> start docker centos-7 + mysql5.7 now,please wait ....'
-docker run --privileged -itd  -v /data/mysql-33306:/data  -v /etc/localtime:/etc/localtime:ro -p 33306:3306  centos-7-mysql /usr/sbin/init
+docker run --privileged -dit  -v /data/mysql-33306:/data  -v /etc/localtime:/etc/localtime:ro -p 33306:3306  centos-7-mysql /usr/sbin/init
 echo '<<< finish start docker centos-7 mysql 5.7,docker exec -it <container_id> /bin/bash'
 exit 0
